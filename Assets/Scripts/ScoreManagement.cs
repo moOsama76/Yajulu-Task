bc06d3dcefb98933406beed9a0f55b8abbf7a889
Mohamed Osama <67656249+moOsama76@users.noreply.github.com>
2022-11-03 14:14:24 +0200

    Update ScoreManagement.cs

--- Assets/Scripts/ScoreManagement.cs
+++ Assets/Scripts/ScoreManagement.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,12 +12,16 @@ public class ScoreManagement : MonoBehaviour
     public GameObject character, finalScore;
     public RectTransform plus1000;
     public int score = 0;
+    Vector2 initialPosition = new Vector2(-200, -200), targetPosition;
     int scoreInUI = 0;
+
     void Start(){
+        targetPosition = new Vector2(gameObject.GetComponent<RectTransform>().anchoredPosition.x+50, gameObject.GetComponent<RectTransform>().anchoredPosition.y-30);
         InvokeRepeating("addScore", 5, 5);    
     }
 
     void addScore(){
+       
         if(character.GetComponent<CharacterMovement>().playerAlive){
             score += 1000;
             StartCoroutine(plus1000Animation());
@@ -24,8 +29,8 @@ public class ScoreManagement : MonoBehaviour
     }
 
     IEnumerator plus1000Animation(){
-        plus1000.DOAnchorPos(new Vector2(200, 0), 0f, true);
-        plus1000.DOAnchorPos(new Vector2(380, 110), 1f, true);
+        plus1000.DOAnchorPos(initialPosition, 0f, true);
+        plus1000.DOAnchorPos(targetPosition, 1f, true);
         yield return new WaitForSeconds(1f);
         StartCoroutine(scoreIncrementAnimation());
         plus1000.DOAnchorPos(new Vector2(5000, 5000), 0f, true);
